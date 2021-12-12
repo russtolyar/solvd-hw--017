3af252eb50c6a1ebae4cc6e2171c3fdd175539c4
rus-sto <rus-sto@tut.by>
2021-12-12 14:32:45 +0300

    corrected comments'

--- src/main/java/com/solved/mvchw017/persistence/Config.java
+++ src/main/java/com/solved/mvchw017/persistence/Config.java
@@ -5,32 +5,29 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-    public static final String DRIVER;
-    public static final String USER;
-    public static final String URL;
-    public static final String PASSWORD;
-    public static final String POOLSIZE;
+    public static String DRIVER;
+    public static String USER;
+    public static String URL;
+    public static String PASSWORD;
+    public static String POOLSIZE;
 
 
     static {
-        DRIVER = getValue("driver");
-        USER = getValue("user");
-        URL = getValue("url");
-        PASSWORD = getValue("password");
-        POOLSIZE = getValue("poolSize");
-
-    }
-
-    public static String getValue(String info) {
         Properties properties = new Properties();
         String pass = "src/main/resources/config.properties";
         try
                 (FileInputStream fileInputStream = new FileInputStream(pass)) {
             properties.load(fileInputStream);
+
+            DRIVER = properties.getProperty("driver");
+            USER = properties.getProperty("user");
+            URL = properties.getProperty("url");
+            PASSWORD = properties.getProperty("password");
+            POOLSIZE = properties.getProperty("poolSize");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return properties.getProperty(info);
     }
 
 }
