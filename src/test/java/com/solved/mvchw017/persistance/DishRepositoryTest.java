3410e5dca8f754242482a2ad484888efc8cbc905
Ruslan <92981991+russtolyar@users.noreply.github.com>
2021-12-29 11:25:32 +0300

    Hw  017 (#10)
    * with mistakes
    
    * clean code without selects
    
    * clean code without selects
    
    * Jdbc seems warkable
    
    * Jdbc and mybatis
    
    * corrected comments'
    
    * TestNG added
    
    Co-authored-by: rus-sto <rus-sto@tut.by>

---
+++ src/test/java/com/solved/mvchw017/persistance/DishRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.solved.mvchw017.persistance;
+
+public class DishRepositoryTest {
+}
---
+++ src/test/java/com/solved/mvchw017/persistance/impl/AddressesRepositoryTest.java
@@ -0,0 +1,106 @@
+package com.solved.mvchw017.persistance.impl;
+
+import com.solved.mvchw017.domain.Address;
+import com.solved.mvchw017.persistence.impl.AddressJDBCRepositoryImpl;
+import com.solved.mvchw017.service.AddressService;
+import com.solved.mvchw017.service.impl.AddressServiceImpl;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AddressesRepositoryTest {
+    private final AddressService addressService;
+
+    public AddressesRepositoryTest() {
+        this.addressService = new AddressServiceImpl();
+    }
+
+    @DataProvider(name = "addressRepositoryTest")
+    public Object[][] provide() {
+        Address addressOne = new Address();
+        Address addressTwo = new Address();
+        addressOne.setCity("Minsk");
+        addressOne.setStreet("White");
+        addressOne.setNumberHouse(11);
+        addressOne.setNumberFlat(22);
+
+        addressTwo.setCity("Minsk");
+        addressTwo.setStreet("Black");
+        addressTwo.setNumberHouse(33);
+        addressTwo.setNumberFlat(44);
+        return new Object[][]{
+                {addressOne},
+                {addressTwo}};
+    }
+
+
+    @Test
+            (testName = " Creation Address in db",
+                    description = " Address created in db",
+            dataProvider = "addressRepositoryTest",
+                    groups = {"create.delete"})
+    public void addressCreationTest(Address address) {
+        AddressJDBCRepositoryImpl  addressJDBCRepository = new AddressJDBCRepositoryImpl();
+
+        Address address1 = addressJDBCRepository.create(address);
+        addressService.create(address);
+        Address addressSelected = addressSelect(address);
+
+        Assert.assertEquals(address1.getCity(), addressSelected.getCity());
+        Assert.assertEquals(address1.getStreet(), addressSelected.getStreet());
+        Assert.assertEquals(address1.getNumberHouse(), addressSelected.getNumberHouse());
+        Assert.assertEquals(address1.getNumberFlat(), addressSelected.getNumberFlat());
+
+    }
+
+
+    @Test(testName = " Selection Address in db",
+            description = "Address selected in db",
+            dataProvider = "addressRepositoryTest")
+    public void addressSelectionTest(Address address) {
+        addressService.create(address);
+        Address addressSelected = addressSelect(address);
+        System.out.println(addressSelected);
+        Assert.assertEquals(address.getCity(), addressSelected.getCity());
+    }
+
+
+    @Test(testName = " Deletion Address from db ",
+            description = " Address Deleted from db ",
+            dataProvider = "addressRepositoryTest",
+            priority = 1,
+            groups = {"create.delete"})
+    public void addressDeletionTest(Address address) {
+        addressService.create(address);
+        addressService.delete(address);
+        Address addressSelected = addressSelect(address);
+        Assert.assertNull(addressSelected.getId());
+        Assert.assertNull(addressSelected.getCity());
+        Assert.assertNull(addressSelected.getStreet());
+
+    }
+
+    @Test(testName = " Updating Address in db",
+            description = " Address Updated in db ",
+            dataProvider = "addressRepositoryTest")
+    public void addressUpdatingTest(Address address) {
+
+        Address addressMine = new Address();
+        addressMine.setCity("55");
+        addressMine.setStreet("66");
+        addressMine.setNumberHouse(77);
+        addressMine.setNumberFlat(88);
+        addressService.update(address, addressMine);
+        Address addressEx = addressSelect(address);
+        Address addressNew = addressSelect(addressMine);
+        Assert.assertNotEquals(addressEx.getCity(), addressNew.getCity());
+        Assert.assertNotEquals(addressEx.getStreet(), addressNew.getStreet());
+        Assert.assertNotEquals(addressEx.getNumberHouse(), addressNew.getNumberHouse());
+        Assert.assertNotEquals(addressEx.getNumberFlat(), addressNew.getNumberFlat());
+    }
+
+
+    private Address addressSelect(Address address) {
+        return addressService.select(address);
+    }
+}
